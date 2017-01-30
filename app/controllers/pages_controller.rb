a186a829b1ba18779be8ce5b7ffe9309c2bc3994
Mike Orr <mikeorr85@gmail.com>
2017-01-29 18:51:37 -0700

    home page and placeholders for other pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,19 @@ class PagesController < ApplicationController
   def home
   end
 
+  def photos
+  end
+
+  def wedding_party
+  end
+
+  def info
+  end
+
+  def things_to_do
+  end
+
+  def registry
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get 'photos', to: "pages#photos", as: "photos"
+  get 'wedding-party', to: "pages#wedding_party", as: "wedding-party"
+  get 'info', to: "pages#info", as: "info"
+  get 'things-to-do', to: "pages#things_to_do", as: "things-to-do"
+  get 'registry', to: "pages#registry", as: "registry"
+
+  resources :invites, only: [:show]
+  resources :rsvps, only: [:create]
 end
