d70cf5e9416983ae3089839fe8d8c1ccdc35a394
Mike Orr <mikeorr85@gmail.com>
2017-02-12 23:25:08 -0700

    prevent duplicate RSVPs

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -2,4 +2,5 @@ class Rsvp < ApplicationRecord
   belongs_to :invite
 
   validates_presence_of :invite
+  validates_uniqueness_of :invite, message: "RSVP already submitted. Please contact JoRae or Mike to change it."
 end
