f21f55e2608df444bc78e53a6725da6206adce98
Mike Orr <mikeorr85@gmail.com>
2017-03-30 10:35:19 -0600

    show counts on admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -2,13 +2,29 @@ ActiveAdmin.register_page "Dashboard" do
 
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
-      end
-    end
+  content only: :index do
+    render 'index'
+  end
+
+  controller do
+     def index
+       @invite_count = Invite.count
+       @invite_guest_count = Invite.sum(:estimated_party_count)
+       @rsvp_count = Rsvp.count
+       @rsvp_guest_count = Rsvp.sum(:guest_count)
+       @vegetarian_count = Rsvp.sum(:vegetarian_count)
+
+       @latest_rsvps = Rsvp.last(10).reverse
+     end
+  end
+
+  # content title: proc{ I18n.t("active_admin.dashboard") } do
+  #   div class: "blank_slate_container", id: "dashboard_default_message" do
+  #     span class: "blank_slate" do
+  #       span I18n.t("active_admin.dashboard_welcome.welcome")
+  #       small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  #     end
+  #   end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -29,5 +45,5 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
+  # end # content
 end
